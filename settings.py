2b5592736d6baba2a408c1a97df61d05b3921788
hollomancer <chollomon@gmail.com>
2014-10-18 15:05:12 -0400

    messagemiddleware

--- settings.py
+++ settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE_CLASSES = (
     #'django.middleware.cache.FetchFromCacheMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     #'django.middleware.sqlprint.SqlPrintingMiddleware',
-
+    'django.contrib.messages.middleware.MessageMiddleware'
     #below is askbot stuff for this tuple
     'askbot.middleware.anon_user.ConnectToSessionMessagesMiddleware',
     'askbot.middleware.forum_mode.ForumModeMiddleware',
